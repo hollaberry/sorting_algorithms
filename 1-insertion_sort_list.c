19d1f099c164bef12d4eef97e4293e4e66fad5fe
hollaberry <you@example.com>
2022-01-11 05:45:03 -0800

    task 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+/**
+ * len_list - returns the length of a linked list
+ * @list: pointer to the list
+ * Return: length of list
+ */
+
+int len_list(listint_t *list)
+{
+int list_len = 0;
+
+while (list)
+{
+list_len++;
+list = list->next;
+}
+return (list_len);
+}
+
+/**
+ * insertion_sort_list - function to implement insertion_sort on a linked list
+ * @list: double pointer to the list to sort
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+  listint_t *current = Null, *first = Null;
+  listint_t *second = Null, *third = Null, *fourth = Null;
+
+  if (!list || !(*list) || len_list(*list) < 2)
+    return;
+
+  current = *list;
+
+  while (current)
+    {
+      if (current->prev && current->n < current->prev->n)
+	{
+	  first = current->prev->prev;
+	  second = current->prev;
+	  third = current;
+	  fourth = current->next;
+
+	  second->next = four;
+
+	  if (fourth)
+	    fourth->prev = second;
+
+	  third->next = second;
+	  third->prev = first;
+
+	  if (first)
+	    first->next = three;
+	  else
+	    *list = third;
+	  second->prev = third;
+	  current = *list;
+	  print_list(*list);
+	  continue;
+	}
+      else
+	current = current->next;
+    }
+}
