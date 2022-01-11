24325688eb0609091acdbc4236aef857f68e5355
hollaberry <you@example.com>
2022-01-11 07:23:20 -0800

    err orut

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,22 +7,22 @@
  */
 void selection_sort(int *array, size_t size)
 {
-  unsigned int i, j, min;
-  int temp;
-  if (size < 2)
-    return;
-  for (i = 0; i < size; i++)
-    {
-      min = i;
-      for (j = i + 1; j < size; j++)
-	{
-	  if (array[j] < array[min])
-	    min = j;
-	}
-      tmp = array[i];
-      array[i] = array[min];
-      array[min] = tmp;
-      if (i != min)
-	print_array(array, size);
-    }
+unsigned int i, j, min;
+int temp;
+if (size < 2)
+return;
+for (i = 0; i < size; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+min = j;
+}
+temp = array[i];
+array[i] = array[min];
+array[min] = temp;
+if (i != min)
+print_array(array, size);
+}
 }
