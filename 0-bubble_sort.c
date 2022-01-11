1d085e213f3676655738ec752072fe6a15437d95
hollaberry <you@example.com>
2022-01-11 01:51:21 -0800

    error out

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
+#include <stdio.h>
+
 /**
  * bubble_sort - function that sorts an array of integers in ascending order
  * @array: The elmements in the array to be sorted
  * @size: size of the array
- * Return: return a sorted array
  */
 
 void bubble_sort(int *array, size_t size)
@@ -13,13 +14,14 @@ int temp;
 
 if (size < 2)
 return;
+
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size  - i - 1; j++)
 {
-if (arrayji] > array[j + 1])
+if (array[j] > array[j + 1])
 {
-temp = array[i];
+temp = array[j];
 array[j] = array[j + 1];
 array[j + 1] = temp;
 print_array(array,size); 
