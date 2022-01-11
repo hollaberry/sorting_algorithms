9ac92ea770c87c81641d4bcea0f9af5f471a2d06
hollaberry <you@example.com>
2022-01-11 06:28:52 -0800

    error out

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -25,41 +25,41 @@ return (list_len);
 
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *current = NULL, *first = NULL;
-  listint_t *second = NULL, *third =NULL, *fourth = NULL;
+listint_t *current = NULL, *first = NULL;
+listint_t *second = NULL, *third = NULL, *fourth = NULL;
 
-  if (!list || !(*list) || len_list(*list) < 2)
-    return;
+if (!list || !(*list) || len_list(*list) < 2)
+return;
 
-  current = *list;
+current = *list;
 
-  while (current)
-    {
-      if (current->prev && current->n < current->prev->n)
-	{
-	  first = current->prev->prev;
-	  second = current->prev;
-	  third = current;
-	  fourth = current->next;
+while (current)
+{
+if (current->prev && current->n < current->prev->n)
+{
+first = current->prev->prev;
+second = current->prev;
+third = current;
+fourth = current->next;
 
-	  second->next = fourth;
+second->next = fourth;
 
-	  if (fourth)
-	    fourth->prev = second;
+if (fourth)
+fourth->prev = second;
 
-	  third->next = second;
-	  third->prev = first;
+third->next = second;
+third->prev = first;
 
-	  if (first)
-	    first->next = third;
-	  else
-	    *list = third;
-	  second->prev = third;
-	  current = *list;
-	  print_list(*list);
-	  continue;
-	}
-      else
-	current = current->next;
-    }
+if (first)
+first->next = third;
+else
+*list = third;
+second->prev = third;
+current = *list;
+print_list(*list);
+continue;
+}
+else
+current = current->next;
+}
 }
